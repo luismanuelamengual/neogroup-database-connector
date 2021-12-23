a0a749124dbd8e5de7fadf325cce16e7e1f5bbc6
Luis Amengual <luismanuelamengual@gmail.com>
2021-12-23 16:25:43 -0300

    Subo cambios menores

--- src/sources/postgres/postgres-data-source.ts
+++ src/sources/postgres/postgres-data-source.ts
@@ -3,6 +3,22 @@ import {Connection} from '../../connection';
 import {PostgresConnection} from './postgres-connection';
 
 export class PostgresDataSource extends DataSource {
+
+    private postgreLib: any;
+    private pool: any;
+
+    constructor() {
+        super();
+        try {
+            this.postgreLib = require("pg");
+        } catch (e) {
+            throw new Error("PostgreSQL module not found. Please install it via \"npm install -S pg\"");
+        }
+        
+        this.pool = new this.postgreLib.Pool();
+        console.log(this.pool);
+    }
+
     public getConnection(): Connection {
         return new PostgresConnection();
     }
