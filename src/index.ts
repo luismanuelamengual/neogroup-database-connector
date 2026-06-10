7b4ea0f04d8f14f79ce15ffd73e8de27373093b0
Luis Amengual <luis.amengual@halopowered.com>
2026-06-10 19:04:33 -0300

    Create sources module

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,6 @@ export * from './DataSet'
 export * from './DataSource'
 export * from './DataTable'
 export * from './DB'
-//Queries
-export * from './query'
-//Entities
 export * from './entities'
-//Postgres
-export * from './sources/postgres'
-//Sqlite
-export * from './sources/sqlite'
-//MySQL
-export * from './sources/mysql'
+export * from './query'
+export * from './sources/'
