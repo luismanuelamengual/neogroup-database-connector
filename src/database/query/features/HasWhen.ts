a5cfca6995489627b997173d812614748863fd58
Luis Amengual <luis.amengual@halopowered.com>
2026-06-13 22:10:03 -0300

    Fix for "when"

--- src/database/query/features/HasWhen.ts
+++ src/database/query/features/HasWhen.ts
@@ -1,5 +1,5 @@
 export abstract class HasWhen<R> {
-  public when(condition: boolean | undefined | null, callback: (query: R) => void): R {
+  public when(condition: any, callback: (query: R) => void): R {
     if (condition) {
       callback(this as unknown as R)
     }
